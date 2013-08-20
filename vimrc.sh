cfeb3f7578c041b0ea203dece466360ef1029bd8
titico <titicosadde@gmail.com>
2013-08-20 21:20:21 +0100

    very simplet setup vimrc.sh

--- vimrc.sh
+++ vimrc.sh
@@ -5,10 +5,13 @@
 	to create the correct links to get a vim configuration
 	running quickly
 REFERENCE
+echo "This will updated all submodules and set the vimrc correctly"
 
 
-echo "Move to home directory"
-cd $HOME
-
-echo "Create symbolic link to the actual vimrc inside .vim"
-ln -s $PWD/vimrc $HOME/.vimrc  
+    echo "initialising all submodules"
+    git submodule init
+    git submodule update
+    echo "Create symbolic link to the actual vimrc inside .vim"
+    echo $PWD
+    echo $HOME
+    ln -s $PWD/vimrc $HOME/.vimrc  
